fd74feb61ccfae7166f4de60b9b58f04a6020377
ashley-lujan <lujanashlet@gmail.com>
2023-02-15 11:18:20 -0700

    fixed some things

--- MadLibs/RunStory.cs
+++ MadLibs/RunStory.cs
@@ -36,28 +36,8 @@ namespace MadLibs
             //TODO; change a lot of the things to static and make sure you pass in a string
             public Story()
             {
-                //name = "";
-                //friend = "";
-                //they = "";
-                //them = "";
-                //nouns = new Queue<string>();
-                //adjectives = new Queue<string>(); 
-                //verbs = new Queue<string>();
-
                 AskQuestions(); 
 
-                //name = Ask("What is your name? \t");
-                //friend = Ask("What is a friend's name \t");
-              
-
-                //they = "she";
-                //them = "her";
-                //nouns = Read_From_Queue("noun", 3);
-                //adjectives = Read_From_Queue("adjective", 3);
-                //verbs = Read_From_Queue("verb", 3);
-                //all_stories = new LinkedList<string>();
-                //all_stories.AddFirst($"Today {name} is at SheTech {they} hopes to {adjectives.Dequeue()} with {friend}. {nouns.Dequeue()} at today's conference {verbs.Dequeue()} to {nouns.Dequeue()} which is {adjectives.Dequeue()} the best. ");
-                //all_stories.Last().
             }
 
             private string Ask(string question)
@@ -84,6 +64,13 @@ namespace MadLibs
                 friend = Ask("What is a friend's name\t");
                 string pronouns = Ask("what are your pronouns? Use she,her format \t");
                 string[] pro = pronouns.Split(",");
+                while (pro.Length < 2)
+                {
+                    Console.WriteLine("\nOops! Trouble reading, please try again!");
+                    pronouns = Ask("what are your pronouns? Use she,her format \t");
+                    pro = pronouns.Split(",");
+                }
+
                 they = pro[0];
                 them = pro[1]; 
 
@@ -98,18 +85,22 @@ namespace MadLibs
             {
                 
                 //int count = 1;
-                int num_of_stories = 2;
+                int num_of_stories = 3;
                 for (int count = 0; count != -1; count++)
                 {
                     if(count % num_of_stories == 0)
                     {
-                        yield return $"Today {name} is at SheTech {they} hopes to {adjectives.Dequeue()} with {friend}. {nouns.Dequeue()} at today's conference {verbs.Dequeue()} to {nouns.Dequeue()} which is {adjectives.Dequeue()} the best. "; 
+                        yield return $"Today {name} is at the major expo {they} hopes to {adjectives.Dequeue()} with {friend}. {nouns.Dequeue()} at today's conference {verbs.Dequeue()} to {nouns.Dequeue()} which is {adjectives.Dequeue()} the best. "; 
                     }
                     else if (count % num_of_stories == 1)
                     {
                         yield return $"Hi {name} and {friend}! We are glad you made it to major expo this year and stopped by our booth! Don't forget to pick up a {nouns.Dequeue()} while you {verbs.Dequeue()} around today. If you happen to find a {adjectives.Dequeue()} {nouns.Dequeue()} be sure to take a selfie with it. Go have a {adjectives.Dequeue()} day!"; 
                     }
-                    
+                    else if (count % num_of_stories == 2)
+                    {
+                        yield return $"Yesterday, {name} went to the store with {friend}! They bought a {adjectives.Dequeue()} {nouns.Dequeue()} and forgot they had to go {verbs.Dequeue()} with their {adjectives.Dequeue()} {nouns.Dequeue()}.";
+                    }
+
                 }
             }
 
@@ -122,7 +113,7 @@ namespace MadLibs
                     Console.WriteLine(story);
                     //wait 10 seconds? 
                     Thread.Sleep(1000);
-                    Console.WriteLine("\n\n\n\n\n\n\n Next questions"); 
+                    Console.WriteLine("\n\n\n\n\n\n\n Do our madlib! \n\n\n\n\n"); 
                     AskQuestions(); 
                 }
 
